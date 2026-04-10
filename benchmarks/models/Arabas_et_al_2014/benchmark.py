002cabb6f10cd263697acb8c250db3d1facbe0ed
Griger5 <griger04@gmail.com>
2026-04-10 23:34:22 +0200

    update: return results of the computation, fix the setup of JAX implementation (missing halos)

--- benchmarks/models/Arabas_et_al_2014/benchmark.py
+++ benchmarks/models/Arabas_et_al_2014/benchmark.py
@@ -2,7 +2,7 @@ from models.Arabas_et_al_2014 import mpdata
 
 solver = None
 
-def setup(metadata: dict = {}):
+def setup(data, metadata: dict = {}):
 	global solver
 	solver = mpdata.solver_donorcell(
 		bcx = mpdata.cyclic,
@@ -16,3 +16,5 @@ def compute(data, metadata: dict = {}):
 	solver.courant(0)[:,:] = data[1][metadata["halo"]:-metadata["halo"], :] 
 	solver.courant(1)[:,:] = data[2][:, metadata["halo"]:-metadata["halo"]]
 	solver.solve(metadata["steps"])
+
+	return solver.state()
