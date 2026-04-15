70171e6d895ab1c7dc074486971bb414359e5d76
Griger5 <griger04@gmail.com>
2026-04-15 20:25:06 +0200

    fix: remove mutable objects from default arguments

--- benchmarks/models/Arabas_et_al_2014/benchmark.py
+++ benchmarks/models/Arabas_et_al_2014/benchmark.py
@@ -2,7 +2,7 @@ from models.Arabas_et_al_2014 import mpdata
 
 solver = None
 
-def setup(data, metadata: dict = {}):
+def setup(data, metadata: dict):
     global solver
     solver = mpdata.solver_donorcell(
         bcx = mpdata.cyclic,
@@ -11,10 +11,11 @@ def setup(data, metadata: dict = {}):
         ny = metadata["size_y"],
     )
 
-def compute(data, metadata: dict = {}):
+def compute(data, metadata: dict):
     solver.state()[:] = data[0]
     solver.courant(0)[:,:] = data[1][metadata["halo"]:-metadata["halo"], :]
     solver.courant(1)[:,:] = data[2][:, metadata["halo"]:-metadata["halo"]]
     solver.solve(metadata["steps"])
 
+def result_to_numpy(data, metadata: dict):
     return solver.state()
--- benchmarks/models/jax-mpdata/benchmark.py
+++ benchmarks/models/jax-mpdata/benchmark.py
@@ -8,13 +8,13 @@ cpu_device = jax.devices("cpu")[0]
 
 psi = None
 
-def setup(data, metadata: dict = {}):
+def setup(data, metadata: dict):
     global psi
     with jax.default_device(cpu_device):
         psi = jnp.zeros((metadata["size_x"] + 2*metadata["halo"], metadata["size_y"] + 2*metadata["halo"]))
         psi = psi.at[metadata["halo"]:metadata["halo"]+metadata["size_x"], metadata["halo"]:metadata["halo"]+metadata["size_y"]].set(data[0])
 
-def compute(data, metadata: dict = {}):
+def compute(data, metadata: dict):
     global psi
     with jax.default_device(cpu_device):
         return solve(psi, data[1], data[2], metadata["steps"], metadata["halo"])
