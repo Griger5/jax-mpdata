c4fa89c1f7a435691a35c4eeda328cf6ec70a1b6
Griger5 <griger04@gmail.com>
2026-05-11 22:43:50 +0200

    update: netcdf now hold the n_iter value, new benchmark data

--- create_data.py
+++ create_data.py
@@ -8,65 +8,38 @@ def gaussian_2d(height, width):
 
     return np.exp(-((x - cx)**2 + (y - cy)**2) / (2 * sigma**2), dtype="float32")
 
+def create_gaussian_benchmark(size_x, size_y, steps, halo, n_iters, name):
+    psi = gaussian_2d(size_x, size_y)
+    Cx = np.full((size_x + 1 + 2 * halo, size_y + 2 * halo), -0.2, dtype="float32")
+    Cy = np.full((size_x + 2 * halo, size_y + 1 + 2 * halo), 0.5, dtype="float32")
+
+    ds = xr.Dataset(
+        {
+            "psi": (("x", "y"), psi),
+            "Cx": (("x_u", "y_u"), Cx),
+            "Cy": (("x_v", "y_v"), Cy),
+        },
+        coords={
+            "x": np.arange(size_x),
+            "y": np.arange(size_y),
+            "x_u": np.arange(Cx.shape[0]),
+            "y_u": np.arange(Cx.shape[1]),
+            "x_v": np.arange(Cy.shape[0]),
+            "y_v": np.arange(Cy.shape[1]),
+        }
+    )
+
+    ds.attrs["steps"] = steps
+    ds.attrs["size_x"] = size_x
+    ds.attrs["size_y"] = size_y
+    ds.attrs["halo"] = halo
+    ds.attrs["n_iters"] = n_iters
+
+    ds.to_netcdf(f"benchmarks/data/{name}.nc", format="NETCDF4")
+
 if __name__ == "__main__":
-    size_x = 200
-    size_y = 300
-    halo = 1
-    
-    psi = gaussian_2d(size_x, size_y)
-    Cx = np.full((size_x + 1 + 2 * halo, size_y + 2 * halo), -0.2, dtype="float32")
-    Cy = np.full((size_x + 2 * halo, size_y + 1 + 2 * halo), 0.5, dtype="float32")
+    create_gaussian_benchmark(200, 300, 500, 1, 1, "gaussian2d_big_upwind")
+    create_gaussian_benchmark(20, 30, 50, 1, 1, "gaussian2d_small_upwind")
 
-    ds = xr.Dataset(
-        {
-            "psi": (("x", "y"), psi),
-            "Cx": (("x_u", "y_u"), Cx),
-            "Cy": (("x_v", "y_v"), Cy),
-        },
-        coords={
-            "x": np.arange(size_x),
-            "y": np.arange(size_y),
-            "x_u": np.arange(Cx.shape[0]),
-            "y_u": np.arange(Cx.shape[1]),
-            "x_v": np.arange(Cy.shape[0]),
-            "y_v": np.arange(Cy.shape[1]),
-        }
-    )
-
-    ds.attrs["steps"] = 500
-    ds.attrs["size_x"] = size_x
-    ds.attrs["size_y"] = size_y
-    ds.attrs["halo"] = halo
-
-    ds.to_netcdf("benchmarks/data/gaussian2d_big.nc", format="NETCDF4")
-
-    size_x = 20
-    size_y = 30
-    halo = 1
-    
-    psi = gaussian_2d(size_x, size_y)
-    Cx = np.full((size_x + 1 + 2 * halo, size_y + 2 * halo), -0.2, dtype="float32")
-    Cy = np.full((size_x + 2 * halo, size_y + 1 + 2 * halo), 0.5, dtype="float32")
-
-    ds = xr.Dataset(
-        {
-            "psi": (("x", "y"), psi),
-            "Cx": (("x_u", "y_u"), Cx),
-            "Cy": (("x_v", "y_v"), Cy),
-        },
-        coords={
-            "x": np.arange(size_x),
-            "y": np.arange(size_y),
-            "x_u": np.arange(Cx.shape[0]),
-            "y_u": np.arange(Cx.shape[1]),
-            "x_v": np.arange(Cy.shape[0]),
-            "y_v": np.arange(Cy.shape[1]),
-        }
-    )
-
-    ds.attrs["steps"] = 50
-    ds.attrs["size_x"] = size_x
-    ds.attrs["size_y"] = size_y
-    ds.attrs["halo"] = halo
-
-    ds.to_netcdf("benchmarks/data/gaussian2d_small.nc", format="NETCDF4")
+    create_gaussian_benchmark(200, 300, 500, 1, 3, "gaussian2d_big_mpdata")
+    create_gaussian_benchmark(20, 30, 50, 1, 3, "gaussian2d_small_mpdata")
